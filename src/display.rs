be50caa2ea900ab792465a17efbd1a1f8d1c9c14
randypalusz <ibanezguy998@gmail.com>
2022-08-07 16:56:50 -0400

    corrected pixel display, properly fits window now

--- src/display.rs
+++ src/display.rs
@@ -1,8 +1,7 @@
 use glutin_window::GlutinWindow as Window;
 use graphics::types::Color;
 use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event_loop::{EventSettings, Events};
-use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::input::RenderArgs;
 use piston::window::WindowSettings;
 
 const NUM_PIXELS_Y: usize = 32;
@@ -13,8 +12,8 @@ type PixelsArray = [Pixel; NUM_PIXELS];
 
 #[derive(Clone, Copy, Debug)]
 pub struct Pixel {
-    center_x: f64,
-    center_y: f64,
+    start_x: f64,
+    start_y: f64,
     state: bool, // false is off, true is on
 }
 
@@ -36,14 +35,18 @@ impl Display {
         let pixel_size: f64 = height / (NUM_PIXELS_Y as f64);
 
         let opengl = OpenGL::V3_2;
+
         let window: Window = WindowSettings::new("CHIP-8", [width, height])
             .graphics_api(opengl)
             .exit_on_esc(true)
+            .resizable(false)
             .build()
             .unwrap();
         let renderer: GlGraphics = opengl_graphics::GlGraphics::new(opengl);
 
-        let pixels: PixelsArray = Display::init_pixels(&pixel_size, &width, &height);
+        let mut pixels: PixelsArray = Display::init_pixels(&pixel_size, &width, &height);
+
+        Display::pixel_test(&mut pixels);
 
         Display {
             width,
@@ -57,30 +60,39 @@ impl Display {
         }
     }
 
-    // TODO: Seems like the pixel initializtion is off - not starting at exactly the right spot
+    //TODO: remove this, just being used to test display for now
+    fn pixel_test(pixels: &mut PixelsArray) {
+        let mut first_state_toggle: bool = false;
+        let mut state_toggle: bool = first_state_toggle;
+
+        for (idx, mut pixel) in pixels.iter_mut().enumerate() {
+            state_toggle = !state_toggle;
+            if idx % NUM_PIXELS_X == 0 {
+                state_toggle = first_state_toggle;
+                first_state_toggle = !first_state_toggle;
+            }
+            pixel.state = state_toggle;
+        }
+    }
+
     fn init_pixels(pixel_size: &f64, window_width: &f64, window_height: &f64) -> PixelsArray {
         let mut pixels: PixelsArray = [Pixel {
-            center_x: 0.0,
-            center_y: 0.0,
+            start_x: 0.0,
+            start_y: 0.0,
             state: false,
         }; NUM_PIXELS];
 
-        let mut current_x: f64 = pixel_size / 2.0;
-        let mut current_y: f64 = pixel_size / 2.0;
-
-        let mut state_toggle: bool = false;
+        let mut current_x: f64 = 0.0;
+        let mut current_y: f64 = 0.0;
 
         for mut pixel in pixels.iter_mut() {
-            state_toggle = !state_toggle;
             if current_x >= *window_width {
-                println!("here: current_x: {current_x}, current_y: {current_y}");
-                current_x = pixel_size / 2.0;
+                current_x = 0.0;
                 current_y += window_height / NUM_PIXELS_Y as f64;
             }
 
-            pixel.center_x = current_x;
-            pixel.center_y = current_y;
-            pixel.state = state_toggle;
+            pixel.start_x = current_x;
+            pixel.start_y = current_y;
 
             current_x += pixel_size;
         }
@@ -96,7 +108,7 @@ impl Display {
             clear(self.off_color, gl);
 
             for pixel in self.pixels {
-                let transform = c.transform.trans(pixel.center_x, pixel.center_y);
+                let transform = c.transform.trans(pixel.start_x, pixel.start_y);
                 let draw_color = if pixel.state {
                     self.on_color
                 } else {
